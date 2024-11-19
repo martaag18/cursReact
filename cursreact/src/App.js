d09da969f57230acbc2d3ce5c77a8e3fbf8508af
Marta Altimir <martaag18@gmail.com>
2024-11-19 20:56:57 +0100

    Prova - "Segona-Rama"

--- cursreact/src/App.js
+++ cursreact/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="color">Hola k ase?</h1>
-      <h1> Hola </h1>
+      <h1> Segona modificació </h1>
       <img src="" alt="" />
     </>
   );
